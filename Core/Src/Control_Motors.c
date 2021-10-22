abb14d8d5eaaec637d29480044eccd12afbe7e9a
Lincoln Wallace <lincolnble@hotmail.com>
2021-10-22 11:35:29 -0300

    Function to drive back in a distance in cm

--- Core/Src/Control_Motors.c
+++ Core/Src/Control_Motors.c
@@ -5,7 +5,6 @@
  *      Author: linco
  */
 
-
 #include "Control_Motors.h"
 #include "gpio.h"
 #include "tim.h"
@@ -19,10 +18,11 @@ Velocidades VEL;
 
 void stop(void);
 void re(double rpm);
+void re_dist(double rpm, double distance);
 void frente(double rpm);
 void direita(double rpm);
 void esquerda(double rpm);
-
+uint8_t dist_2_pulso(double dist);
 void speed(double DIR, double ESQ);
 void adjust_PWM(double DC, uint8_t motor);
 
@@ -57,6 +57,10 @@ uint8_t angle_2_pulso(uint8_t angle){
 }
 uint8_t pulso_2_angle(uint8_t pulse){
 	 return (uint8_t)(pulse%360)*(9.0/4.0);
+}
+
+uint8_t dist_2_pulso(double dist){
+	return (uint8_t)(dist+1);
 }
 
 void init_motors(double rpm){
@@ -104,6 +108,27 @@ void esquerda(double rpm){
 	IN2_OFF;
 	IN3_ON;
 	IN4_OFF;
+}
+
+void re_dist(double rpm, double distance){
+	speed(rpm, rpm);
+	__disable_irq();
+	uint32_t pulse_D_old = get_pulso_ang(M_DIR);
+	uint32_t pulse_E_old = get_pulso_ang(M_ESQ);
+	__enable_irq();
+	uint32_t pulse_D_new;
+	uint32_t pulse_E_new;
+	do {
+		IN1_OFF;
+		IN2_ON;
+		IN3_OFF;
+		IN4_ON;
+		__disable_irq();
+		pulse_D_new = get_pulso_ang(M_DIR);
+		pulse_E_new = get_pulso_ang(M_ESQ);
+		__enable_irq();
+	} while (((pulse_D_new - pulse_D_old)< dist_2_pulso(distance)) &&
+			((pulse_E_new - pulse_E_old)< dist_2_pulso(distance)));
 }
 
 void rot_dir(double rpm, uint8_t angle){
